2455a4a049a2f8a09a27b1f110466a849234adfa
fanux <fhtjob@hotmail.com>
2017-11-30 20:02:48 +0800

    add

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-./kubeinit gen --master 10.1.245.93 --master 10.1.245.92  --master 10.1.245.95  \
-    --etcd  10.1.245.93  --etcd 10.1.245.92 --etcd 10.1.245.95 \
+./kubeinit gen --master 10.1.245.93 --master 10.1.245.92  --master 10.1.245.94  \
+    --etcd  10.1.245.93  --etcd 10.1.245.92 --etcd 10.1.245.94 \
     --loadbalance 10.1.245.93 
