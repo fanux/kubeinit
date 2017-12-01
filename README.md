37e437c5ebb8deacfd2a33c703d3dfa3e84f2794
fanux <fhtjob@hotmail.com>
2017-12-01 09:02:23 +0800

    add

--- README.md
+++ README.md
@@ -10,8 +10,12 @@ $ kubeinit gen --master 1.1.1.2 --master 1.1.1.3 --master 1.1.1.4 \
 
 ## install etcd and masters
 ```
-$ kubeinit apply
+$ kubeinit apply -bie
 ```
+开关：
+* -b 只初始化环境，拷贝bin文件，配置文件，导入镜像等
+* -i 执行kubeadm init命令
+* -e 启动etcd
 
 ## featrures
 - [x] 自动生成多etcd节点compose文件
--- cmd/apply.go
+++ cmd/apply.go
@@ -22,9 +22,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// appyCmd represents the appy command
-var appyCmd = &cobra.Command{
-	Use:   "appy",
+// applyCmd represents the apply command
+var applyCmd = &cobra.Command{
+	Use:   "apply",
 	Short: "init env and appy kubeadmin init",
 	Long: `初始化环境，拷贝bin程序，配置文件，加载镜像，执行kubeadm, 初始化其它节点
 	init env, copy binarys config files, load docker images, exec kubeadm init and setup other master nodes`,
@@ -36,18 +36,19 @@ var appyCmd = &cobra.Command{
 }
 
 func init() {
-	RootCmd.AddCommand(appyCmd)
+	RootCmd.AddCommand(applyCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// appyCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// applyCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// appyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	appyCmd.Flags().BoolVarP(&define.InitBaseEnvironment, "init-base-env", "b", false, "init base environment, close firewalld selinux swap, copy bin and configs, load docker images")
-	appyCmd.Flags().BoolVarP(&define.InitKubeadm, "init-kubeadm", "i", false, "exec kubeadm init")
-	appyCmd.Flags().BoolVarP(&define.StartEtcdCluster, "start-etcd", "e", false, "docker compose up etcd compose files")
+	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	applyCmd.Flags().BoolVarP(&define.InitBaseEnvironment, "init-base-env", "b", false, "init base environment, close firewalld selinux swap, copy bin and configs, load docker images")
+	applyCmd.Flags().BoolVarP(&define.InitKubeadm, "init-kubeadm", "i", false, "exec kubeadm init")
+	applyCmd.Flags().BoolVarP(&define.StartEtcdCluster, "start-etcd", "e", false, "docker compose up etcd compose files")
+
 }
