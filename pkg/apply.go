84ff3413d0f9d334cd10210b734ee820c6e42599
fanux <fhtjob@hotmail.com>
2017-12-12 16:14:45 +0800

    add

--- pkg/apply.go
+++ pkg/apply.go
@@ -188,7 +188,6 @@ func Apply() {
 		//apply join commands
 		for _, ip := range define.KubeFlags.NodeIPs {
 			go func(ip string) {
-				execSSHCommand(define.User, define.Password, ip, "echo 123\necho 4565")
 				execSSHCommand(define.User, define.Password, ip, initbasesh)
 				execSSHCommand(define.User, define.Password, ip, joinCmd)
 			}(ip)
@@ -196,6 +195,7 @@ func Apply() {
 	}
 
 	if define.Distribute {
+		execSSHCommand(define.User, define.Password, "10.1.245.92", "echo 123\necho 4565")
 		distributeFiles()
 	}
 
