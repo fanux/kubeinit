3033d4a85e93cc663599de201163d3da9f20798e
fanux <fhtjob@hotmail.com>
2018-01-25 17:23:47 +0800

    add

---
+++ temp.sh
@@ -0,0 +1,4 @@
+./kubeinit gen --master 172.21.161.28  --master 172.21.161.29  --master 172.21.161.30 \
+   --etcd 172.21.161.28  --etcd 172.21.161.29  --etcd 172.21.161.30 \
+    --version v1.9.2  \
+    --loadbalance 172.21.161.28
